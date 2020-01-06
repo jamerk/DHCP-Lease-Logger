c6c04a7517f5e52682b2e3e0d33346cf2746a523
Johnny A <39755359+ITGoon@users.noreply.github.com>
2020-01-05 23:36:23 -0800

    Fix
    Fixed directory issue

--- Windows DHCP Server/winlogleases.sh
+++ Windows DHCP Server/winlogleases.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Path to where this script is located
+path=/root/Device_Logger/
+
 # Time Keeper Start
 datestart=$(date +%S)
 
@@ -14,13 +17,14 @@ exec 1>>/var/log/winlogleasesdb.log 2> /dev/null
 
 
 # Remove quotes in file
-sed -e 's/"//g' dhcpleases.csv > dhcpleasesuno.csv
+sed -e 's/"//g' /root/Device_Logger/dhcpleases.csv > /root/Device_Logger/dhcpleasesuno.csv
 
 # Remove domain name from hostnames
-sed -e 's/.Your.Domain//g' dhcpleasesuno.csv > dhcpleasesdo.csv
+sed -e 's/.infineonrw.internal//g' /root/Device_Logger/dhcpleasesuno.csv > /root/Device_Logger/dhcpleasesdo.csv
+sed -e 's/.domain//g' /root/Device_Logger/dhcpleasesdo.csv > /root/Device_Logger/dhcpleasesmod.csv
 
 # Get each MAC address from column in the CSV
-dwinmacs=( $(cut -d ',' -f2 dhcpleasesmod.csv ) )
+dwinmacs=( $(cut -d ',' -f2 /root/Device_Logger/dhcpleasesmod.csv ) )
 
 # Replace the dashes in the MAC addresses with colons
 macount=0
@@ -37,13 +41,13 @@ while IFS="" read -r winhost || [ -n "$winhost" ]
 do
   winhosts[winhcount]=$(grep -oP '.*(?=,[0-9a-f]{1,2}([\.:-])(?:[0-9a-f]{1,2}\1){4}[0-9a-f]{1,2})' <<< $winhost)
   winhcount=$((winhcount + 1))
-done < dhcpleasesmod.csv
+done < /root/Device_Logger/dhcpleasesmod.csv
 
 # Get each IP address from column in the CSV
-winips=( $(cut -d ',' -f3 dhcpleasesmod.csv ) )
+winips=( $(cut -d ',' -f3 /root/Device_Logger/dhcpleasesmod.csv ) )
 
 # Had to add underscores to lease expiration date or regex will split date/time
-sed -e 's/\s\+/_/g' dhcpleasesmod.csv > dhcpleasesfinal.csv
+sed -e 's/\s\+/_/g' /root/Device_Logger/dhcpleasesmod.csv > /root/Device_Logger/dhcpleasesfinal.csv
 
 # Get each lease expiration date from column in CSV
 # Cut also was not working here
@@ -52,7 +56,7 @@ while IFS="" read -r winexp || [ -n "$winexp" ]
 do
   winexps[winetcount]=$(grep -oP '(\d{2}\/\d{2}\/\d{4}_)(\d{1,2}:\d{2}:\d{2}_)(AM|PM)' <<< $winexp)
   winetcount=$((winetcount + 1))
-done < dhcpleasesfinal.csv
+done < /root/Device_Logger/dhcpleasesfinal.csv
 
 
 # Loop through each array and create a new array with part of our SQL query
@@ -64,13 +68,13 @@ do
 done
 
 # Insert each row of values from the above array into the table of devices
-for i in "${winfinal[@]}"; do echo "INSERT INTO devices (mac, datetime, hostname, ip) values ($i);" | mysql -u YOUR-SQL-USER -pYOUR-SQL-USER-PASSWORD devicedb; done
+for i in "${winfinal[@]}"; do echo "INSERT INTO devices (mac, datetime, hostname, ip) values ($i);" | mysql -u deb_sql -pd4nk0v1510n45 devicedb; done
 
 # Re-count the ID column
-echo "SET @count = 0; UPDATE devices SET devices.id = @count:= @count + 1; ALTER TABLE devices AUTO_INCREMENT = 1;" | mysql -u YOUR-SQL-USER -pYOUR-SQL-USER-PASSWORD devicedb;
+echo "SET @count = 0; UPDATE devices SET devices.id = @count:= @count + 1; ALTER TABLE devices AUTO_INCREMENT = 1;" | mysql -u deb_sql -pd4nk0v1510n45 devicedb;
 
 # Mark empty datetime values as Reservations
-echo "UPDATE devices SET datetime='Reservation' WHERE datetime='';" | mysql -u YOUR-SQL-USER -pYOUR-SQL-USER-PASSWORD devicedb;
+echo "UPDATE devices SET datetime='Reservation' WHERE datetime='';" | mysql -u deb_sql -pd4nk0v1510n45 devicedb;
 
 # Set time for the log
 logtime=$(date)
@@ -84,7 +88,7 @@ echo $logtime" This script took" "$(($dateend-$datestart))" "Seconds to run and 
 echo "Including Reservations!"
 
 # Clean up the mess I made
-rm dhcpleasesuno.csv
-rm dhcpleasesdo.csv
-rm dhcpleasesmod.csv
-rm dhcpleasesfinal.csv
+rm /root/Device_Logger/dhcpleasesuno.csv
+rm /root/Device_Logger/dhcpleasesdo.csv
+rm /root/Device_Logger/dhcpleasesmod.csv
+rm /root/Device_Logger/dhcpleasesfinal.csv
